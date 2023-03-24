6800985743f4f286607b5d1af1e402761c352cbc
Tanvishinde03 <shindetanvi0203@gmail.com>
2023-03-24 20:43:14 +0530

    [Tanvi] Add : UC1 - Loading Indian State Census Information From CSV File

---
+++ IndianStateCensusAnalyserProblem/CsvStateCensus.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    public class CsvStateCensus
+    {
+        public void ReadStateCensusData(String filePath)
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<StateCensusModel>().ToList();
+                    foreach (var record in records)
+                    {
+                        Console.WriteLine($"{record.State} {record.Population} {record.AreaInSqKm} {record.DensityPerSqKm}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IndianStateCensusAnalyserProblem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string censusfilePath = "F:\\RFP-256\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\IndianState\\StateCensusData.csv";
+            CsvStateCensus csv = new CsvStateCensus();
+            csv.ReadStateCensusData(censusfilePath);
+            Console.ReadLine();
+        }
+    }
+}
