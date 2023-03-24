ef4aa73feb0207d11faaae974b95d72ac2cf561a
Tanvishinde03 <shindetanvi0203@gmail.com>
2023-03-24 23:21:30 +0530

    [Tanvi] Add : Modified to Incorrect File Path Returns Custom Exception

---
+++ IndianStateCensusAnalyserProblem/IndianStateCensusAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyserProblemE
+{
+    public class IndianStateCensusAnalyser
+    {
+        public class IndianStateCensusException : Exception
+        {
+            public enum IndianStateExceptionType
+            {
+                CSV_FILE_IS_INCORRECT
+            }
+            public IndianStateExceptionType indianStateExceptionType;
+            public IndianStateCensusException(IndianStateExceptionType indianStateExceptionType, string message) : base(message)
+            {
+                this.indianStateExceptionType = indianStateExceptionType;
+            }
+        }
+    }
+}
--- IndianStateCensusTestCase/UnitTest1.cs
+++ IndianStateCensusTestCase/UnitTest1.cs
@@ -1,10 +1,12 @@
 using IndianStateCensusAnalyserProblem;
+using static IndianStateCensusAnalyserProblemE.IndianStateCensusAnalyser;
 
 namespace IndianStateCensusTestCase
 {
     public class Tests
     {
         public static string path = @"F:\RFP-256\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\IndianState\StateCensusData.csv";
+        public static string incorrectStatePath = @"F:\RFP-256\IndianStateCensusAnalyserProblem\IndianStateCensusAnalyserProblem\IndianState\StateCensusData.csv";
         StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
         CsvStateCensus csvState = new CsvStateCensus();
         [Test]
@@ -12,6 +14,19 @@ namespace IndianStateCensusTestCase
         {
             Assert.AreEqual(stateCensusAnalyser.ReadStateCensusData(path), csvState.ReadStateCensusData(path));
         }
+        [Test]
+        public void GivenIncorrectCSVFilePath_ShouldReturnCustomException()
+        {
+            try
+            {
+                int record = stateCensusAnalyser.ReadStateCensusData(incorrectStatePath);
+            }
+            catch (IndianStateCensusException ex)
+            {
+                Assert.AreEqual(ex.Message, "Incorrect file path");
+            }
+        }
     }
+}
     
-    }
+    
